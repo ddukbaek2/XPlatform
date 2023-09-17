d1fc3dd6e52317f5c547856a057a3f274ad0325d
dagraac <dagraac@naver.com>
2023-09-17 12:08:37 +0900

    work

---
+++ Build.bat
@@ -0,0 +1,5 @@
+mkdir build
+cd build
+cmake -G "Visual Studio 17 2022" -DCMAKE_C_COMPILER="C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.33.31629\bin\Hostx64\x64\cl.exe" -DCMAKE_CXX_COMPILER="C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.33.31629\bin\Hostx64\x64\cl.exe" ..
+
+cd ..
---
+++ XPlatform.h
@@ -0,0 +1,14 @@
+﻿// XPlatform.h: 표준 시스템 포함 파일
+// 또는 프로젝트 특정 포함 파일이 들어 있는 포함 파일입니다.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: 여기서 프로그램에 필요한 추가 헤더를 참조합니다.
+
+// CMake 기반이므로 다운로드 필수.
+// 윈도우 : https://cmake.org/download/
+// 맥 : brew install cmake
+// 리눅스 (우분투) : sudo apt install cmake
+// 리눅스 (페도라) : sudo dnf install cmake
