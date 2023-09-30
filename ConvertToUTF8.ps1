c09c0129bd2b1393ebceb447d22fc96bb36f02d8
dagraac <dagraac@naver.com>
2023-09-30 23:23:47 +0900

    Update ConvertToUTF8.ps1

--- ConvertToUTF8.ps1
+++ ConvertToUTF8.ps1
@@ -1,3 +1,4 @@
+# 현재디렉토리부터 모든 하위디렉토리에 있는 cpp와 h를 utf8(no-bom) 으로 저장.
 Get-ChildItem -Recurse -Include *.cpp,*.h | ForEach-Object {
     $content = Get-Content $_.FullName
     Set-Content -Encoding utf8 -Path $_.FullName -Value $content
